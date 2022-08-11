5c84b8f59429b82c784b4394c1ee8331b6ecc923
Min <biffboffkev@gmail.com>
2022-08-11 17:25:38 -0400

    commit tree 88b7e3e0fbfa3c12c493477c794e606e5ac3898b

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
-
+document.getElementById("modal").classList.add("hidden");
+let hearts = document.getElementsByClassName("like-glyph");
+for(let i =0; i<hearts.length; i++){
+  hearts[i].addEventListener('click', (event)=>{
+    // console.log(event.target.textContent);
+    // debugger;
+    if(event.target.textContent === EMPTY_HEART){
+      mimicServerCall()
+      .then(()=>{
+        hearts[i].textContent = FULL_HEART;
+        hearts[i].classList.remove("activated-heart");
+      })
+      .catch(()=>{
+        document.getElementById("modal").classList.remove("hidden");
+        setTimeout(()=>{document.getElementById("modal").classList.add("hidden");},3000);
+      });
+    }else if(event.target.textContent===FULL_HEART){
+        hearts[i].textContent = EMPTY_HEART;
+        hearts[i].classList.add("activated-heart");
+    } 
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
